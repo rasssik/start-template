67e9a63ea1c10ecfe42402fb97ad687caae35658
yasell <raman.yasel@yahoo.com>
2016-11-21 14:15:16 +0300

    upgrade v0.1

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ $(document).ready(function ($) {
 	pageWidget(['index', 'blog', 'news', 'about_us', 'contact']);
 	getAllClasses('html','.elements_list');
 });
+
+// back to top
+$("#back-top").hide();
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 100) {
+		$("#back-top").fadeIn();
+	} else {
+		$("#back-top").fadeOut();
+	}
+});
+
+$("#back-top").click(function() {
+	$("body,html").animate({
+		scrollTop: 0
+	}, 500);
+	return false;
+});
